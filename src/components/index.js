ad9784cbaba2ca16b446508ba699e492e9146aff
MuhammadHarisRaza <razaharis132@gmail.com>
2022-11-09 03:57:14 +0500

    feat: stats section has been added
    A stats section has been added to show the number game of the company.

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Hero from "./Hero";
 import Info from "./Info";
 import InfoCard from "./InfoCard";
 import AboutUs from "./AboutUs";
+import Stats from "./Stats";
 
 export {
     Navbar,
     Hero,
     Info,
     InfoCard,
-    AboutUs
+    AboutUs,
+    Stats
 }
